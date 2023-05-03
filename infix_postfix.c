88c193b0609ac7841029a819db11e678bff164f8
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2023-05-03 09:19:56 -0700

    Add files via upload

---
+++ infix_postfix.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<string.h>
+int prec(char ch)
+{
+	switch(ch)
+	{
+		case '^':return 3;
+		case '*':return 2;
+		case '/':return 2;
+		case '+':return 1;
+		case '-':return 1;
+		default:return 0;
+	}
+}
+char *infix_postfix(char *infix)
+{
+	int i,j=0,top=-1,n;
+	n=strlen(infix);
+	char st[n],op;
+	static char postfix[100];
+	for(i=0;i<n;i++)
+	{
+		if(prec(infix[i]))
+		{
+			//...operator
+			if(top==-1)
+			{
+				st[++top]=infix[i];
+			}
+			else if(prec(infix[i]>prec(st[top])))
+			{
+				st[++top]=infix[i];
+			}
+			else 
+			{
+				while(top!=-1 && prec(infix[i])<=prec(st[top]))
+				{
+					op=st[--top];
+					postfix[j++]=op;
+				}
+				st[++top]=infix[i];
+			}
+			
+		}
+		else
+		{
+			//....operend
+			postfix[j]=infix[i];
+			j++;
+		}
+	}
+	for(i=top;i>=0;i--)
+	{
+		postfix[j++]=st[i];
+	}
+	return postfix;
+}
+int main()
+{
+	char infix[100],*postfix;
+	scanf("%[^\n]s",infix);
+	postfix=infix_postfix(infix);
+	printf("%s",postfix);
+	return 0;
+}
