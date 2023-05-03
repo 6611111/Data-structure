606a99cc00073dbfcbcd39edb4c35706d690855c
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2023-05-03 09:20:34 -0700

    Add files via upload

---
+++ insertion_sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void insertion_sort(int *a,int n)
+{
+	int i,j,key;
+	for(i=1;i<n;i++)
+	{
+		key=a[i];
+		j=i-1;
+		while(j>=0 && a[j]>key)
+		{
+			a[j+1]=a[j];
+			j=j-1;
+		}
+		a[j+1]=key;
+	}
+}
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	insertion_sort(a,n);
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
