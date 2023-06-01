34acc43febf11424be549f7c9cc51bbe12097324
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2023-05-31 22:49:50 -0700

    Add files via upload

---
+++ mj.cpp
@@ -0,0 +1,15 @@
+/*
+AVL Tree
+     Balancing factor 
+    difference between depth of left-depth of right (0 -1 1) 
+Rotations:
+1) left rotation
+2) Right rotation
+
+4 cases:
+      right right case ---->left rotation
+      left left case ----> right rotation
+      right left case --->right rotation and left rotation
+      left right case --->lrft rotation and right rotation
+*/
+
