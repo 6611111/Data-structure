e9e8032a392353d2880cf3662b28c4c4d0765b30
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2023-05-31 22:46:39 -0700

    Add files via upload

--- binary_tree_using_linkedlist.c
+++ binary_tree_using_linkedlist.c
@@ -6,12 +6,81 @@ struct NodeLL{
 };
 typedef struct NodeLL NODELL;
 NODELL *head=NULL,*temp,*NNLL;
+struct NodeT{
+	struct NodeT *left;
+	int data;
+	struct NodeT *right;
+};
+typedef struct NodeT NODET;
+NODET *root=NULL,*NNT,*queue[100];
+
 NODELL * newnodeLL(int val)
 {
 	NNLL=(NODELL *)malloc(sizeof(NODELL));
 	NNLL->data=val;
 	NNLL->next=NULL;
 	return NNLL;
+}
+/*
+f=0 and r=0
+                     f                            r
+  1000              2000          3000          4000         5000            6000
+  
+2000 10 3000        N 20 N       N 30 N         L D R       L D R           L D R
+10                    20           30            40          50              60
+head                                            temp
+
+*/
+NODET * newnodeTree(int val)
+{
+	NNT=(NODET *)malloc(sizeof(NODET));
+	NNT->data=val;
+	NNT->left=NULL;
+	NNT->right=NULL;
+	return NNT;
+}
+NODET * constructTree(NODELL *head)
+{
+	int f=0,r=0;
+	temp=head;
+	NNT=newnodeTree(temp->data);
+	queue[r]=NNT;
+	while(1)
+	{
+		if(temp->next!=NULL)
+		{
+			NNT=newnodeTree(temp->next->data);
+			queue[++r]=NNT;
+			queue[f]->left=NNT;
+			temp=temp->next;
+		}
+		else
+		{
+			return queue[0];
+		}
+		if(temp->next)
+		{
+			NNT=newnodeTree(temp->next->data);
+			queue[++r]=NNT;
+			queue[f]->right=NNT;
+			temp=temp->next;
+		}
+		else
+		{
+		  return queue[0];
+		}
+		f++;
+	}
+}
+void inorder(NODET *root)
+{
+	if(!root)
+	{
+		return;
+	}
+	inorder(root->left);
+	printf("%d ",root->data);
+	inorder(root->right);
 }
 void createLL(int val)
 {
@@ -30,7 +99,7 @@ void createLL(int val)
 		temp->next=NNLL;
 	}	
 }
-void display()
+/*void display()
 {
 	temp=head;
 	while(temp)
@@ -39,7 +108,7 @@ void display()
 		printf("%d %d %d\n",temp,temp->data,temp->next);
 		temp=temp->next;
 	}
-}
+}*/
 int main()
 {
 	int n,i,val;
@@ -49,5 +118,7 @@ int main()
 		scanf("%d",&val);  // 10 20 30 40  50
 		createLL(val);  //createLL(10)
 	}
-	display();
+	//display();
+	root=constructTree(head);    // first node of 
+	inorder(root);
 }
